c3b83aa7d706277a940f81a3e7790a497d458713
Matheus <matheusfreitasbsi@gmail.com>
2023-02-14 16:28:28 -0300

    Adicionado o Dapper.Contrib para realizar buscar utilizando métodos e substituir o uso das querys

---
+++ blog/projeto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Dapper.Contrib;
+using Projeto.Model;
+using Dapper.Contrib.Extensions;
+
+var connection = new SqlConnection("Server=localhost,1433;Database=Blog;Integrated Security=true");
+
+using (connection)
+{
+    ReadUsers(connection);
+}
+
+static void ReadUsers(SqlConnection connection)
+{
+    var users = connection.GetAll<User>();
+
+    foreach (var user in users)
+    {
+        Console.WriteLine(user);
+    }
+}
+// static void InsertUser()
+// {
+//     using (var connection = new SqlConnection(connectionString))
+//     {
+//         User user = new User();
+
+//         var rows = connection.Insert<User>(user);
+
+//         Console.WriteLine(rows);
+
+//     }
+// }
+
+
